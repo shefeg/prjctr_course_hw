2eff39dc2042ca5f1622a842807f1ce4ec2cf894
Alexey Ignatenko <aleksey.ignatenko@amo.tech>
2023-11-18 18:28:20 +0200

    add pointer

--- gocourse03/hw/main.go
+++ gocourse03/hw/main.go
@@ -128,16 +128,16 @@ func rememberNameChoice(remember bool, men Human) {
 	}
 }
 	
-func findBagChoice(bagFind bool, men Human, bag Backpack) {
+func findBagChoice(bagFind bool, men Human, bag *Backpack) {
 	men.Bag = bagFind
 	if men.Bag {
 		bag.Matches.Exist = true
 		bag.Flashlight.Exist = true
 		bag.Knife.Exist = true
-		men.FindBag(bag)
+		men.FindBag(*bag)
 	} else {
 		men.Bag = false
-		men.FindBag(bag)
+		men.FindBag(*bag)
 	}
 }
 	
@@ -218,7 +218,7 @@ func main() {
 		Bag: false,
 	}
 	cave := &Cave{"big cage entrance", true}
-	bag := Backpack{
+	bag := &Backpack{
 		Size: 10,
 		Matches: &Matches{"Matches", false},
 		Flashlight: &Flashlight{"Flashlight", false},
@@ -284,7 +284,7 @@ func main() {
 			case "rememberNameChoice":
 				v["choice"].(func (remember bool, men Human))(true, *men)
 			case "findBagChoice":
-				v["choice"].(func (bagFind bool, men Human, bag Backpack))(true, *men, bag)
+				v["choice"].(func (bagFind bool, men Human, bag Backpack))(true, *men, *bag)
 			case "caveChoice":
 				v["choice"].(func (caveDark bool, men Human, cave Cave))(true, *men, *cave)
 			case "roadChoice":
@@ -305,7 +305,7 @@ func main() {
 			case "rememberNameChoice":
 				v["choice"].(func (remember bool, men Human))(false,*men)
 			case "findBagChoice":
-				v["choice"].(func (bagFind bool, men Human, bag Backpack))(false, *men, bag)
+				v["choice"].(func (bagFind bool, men Human, bag Backpack))(false, *men, *bag)
 			case "caveChoice":
 				v["choice"].(func (caveDark bool, men Human, cave Cave))(false, *men, *cave)
 			case "roadChoice":
