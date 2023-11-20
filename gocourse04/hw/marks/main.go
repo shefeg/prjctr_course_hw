160d35659d3c2e7ec64fc5c26e2ea10168f01b28
Alexey Ignatenko <aleksey.ignatenko@amo.tech>
2023-11-20 10:53:27 +0200

    add empty slice validation

--- gocourse04/hw/marks/main.go
+++ gocourse04/hw/marks/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
     "fmt"
+    "os"
 )
 
 func calcAvgMark (marks []float64) float64 {
     var sum float64
+    if len(marks) == 0 {
+        fmt.Println("No marks found. Insert marks into slice.")
+        os.Exit(0)
+    }
     for _, v := range marks {
         sum += v
     }
