4181772bfd6cba845e07b0aab1b85ab1c7dbd2ed
Alexey Ignatenko <aleksey.ignatenko@amo.tech>
2023-11-25 12:44:55 +0200

    update main

--- gocourse05/hw/main.go
+++ gocourse05/hw/main.go
@@ -147,7 +147,7 @@ func main() {
 	field := make(map[Key]string)
 	printField(true, field, fieldSize)
 	fmt.Println()
-	for i := 1; i <= fieldSize*3; i++ {
+	for i := 1; i <= fieldSize * fieldSize; i++ {
 		if i%2 != 0 {
 			player := 1
 			fmt.Println("Player", player)
